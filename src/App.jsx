e7e55269a33b37325b4cba1efce905530cc13f20
Carina Fernandez <carifer411@gmail.com>
2024-06-13 00:35:53 -0300

    PreEntrega1+Fernandez

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div>
       <Navbar />
-      <ItemListContainet greeting={"Hola como estas?"} />
+      <ItemListContainet greeting={"Hola, como estas?"} />
     </div>
   );
 }
